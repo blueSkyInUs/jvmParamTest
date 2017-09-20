2af33da6ac267448356b5633c66b698216b07b3b
lesson <Lesson.lai@bluepay.asia>
2017-09-20 12:01:35 +0800

    init

---
+++ src/main/java/OOMTest.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lesson on 2017/9/20.
+ */
+public class OOMTest {
+
+    public static void main(String[] args) {
+
+
+        List<String> list=new ArrayList<String>();
+        list.add("hello");
+
+
+
+
+    }
+}
